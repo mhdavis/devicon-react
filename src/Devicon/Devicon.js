2a3d9fda94b659501f6898d0b5179f1e6186c0a8
Michael Davis <mhdavis1993@gmail.com>
2017-10-10 23:38:48 -0400

    Deleted unnecessary files + add Devicon component
    - App.js: tested out component functionality

---
+++ src/Devicon/Devicon.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import 'devicon';
+
+const Devicon = (props) => {
+  let style = {
+    fontSize : "5rem"
+  };
+
+  if (props.style) { style = props.style; }
+  if (props.color) { style.color = props.color; }
+  if (props.size) { style.fontSize = props.size; }
+
+  let format = `devicon-${props.name}-${props.logoStyle}`;
+
+  /* Wordmark Boolean */
+  if (props.wordmark) { format += '-wordmark'; }
+  if (!style.color) { format += " colored"; }
+
+  return (
+    <div>
+      <i className={`${format} ${props.iconClass}`} style={style}></i>
+    </div>
+  );
+}
+
+export default Devicon;
